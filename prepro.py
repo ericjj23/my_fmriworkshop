7e6a6bbcf789afdb3be15a021ab95e6cb2ee6a91
FSL User <fsluser@localhost.localdomain>
2017-12-12 16:02:42 +0000

    grool

--- prepro.py
+++ prepro.py
@@ -6,3 +6,17 @@ Created on Tue Dec 12 15:54:01 2017
 @author: fsluser
 """
 
+import glob
+import os
+#import pdb
+import pandas
+import argparse
+  
+
+def prepro(basedir):
+    print('thing thing thing '+basedir)
+def main():
+    basedir='/home/fsluser/Desktop/ds000030_R1.0.5'
+    prepro(basedir)    
+main()
+     
